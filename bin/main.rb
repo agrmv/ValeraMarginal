3381771b2089561a2b1df631cc05d46665276b39
Aleksey Gromov <gromov-aleksey@outlook.com>
2019-10-20 20:21:48 +0700

    Рефакторинг считыывания/записи состояния Валеры, используя серриализацию/десерриализацию

--- bin/main.rb
+++ bin/main.rb
@@ -1,28 +1,22 @@
+# frozen_string_literal: true
+
 require_relative '../lib/condition'
 require_relative '../lib/valera'
 require_relative '../lib/states/states'
 require_relative '../lib/effect'
 require_relative '../lib/action'
-require 'yaml'
 
 states = States.new('lib/states/saved_states.yml')
 load_states = states.load
-valera = Valera.new(health: load_states['health'], mana: load_states['mana'], fun: load_states['fun'],
-                    fatigue: load_states['fatigue'], money: load_states['money'])
-puts valera.health
-valera.health = 200
-puts valera.health
+valera = Valera.new(health: load_states.health, mana: load_states.mana, fun:
+    load_states.fun, fatigue: load_states.fatigue, money: load_states.money)
 
-states.save(valera)
-# cond = Condition.new(field: 'health', operator: '+', value: 200)
-# puts cond.valid?(valera)
-# valera.health=(200)
-# puts valera.health
+cond = Condition.new(field: 'health', operator: '>', value: 200)
+puts cond.valid?(valera)
 
 cond = Condition.new(field: 'health', operator: '>', value: 50)
 effect = Effect.new('health', '-', 60)
 action = Action.new(conds: [cond], effects: [effect])
 action.run valera
 puts valera.health
-t = valera.to_yaml
-puts t
+states.save valera
--- lib/states/states.rb
+++ lib/states/states.rb
@@ -8,16 +8,12 @@ class States
   end
 
   def load
-    YAML.safe_load(File.open(@file, 'r'))
+    YAML.load_file(File.open(@file))
   end
 
   def save(valera)
-    File.open(@file, 'w') do |file|
-      file.puts("health:  #{valera.health}")
-      file.puts("mana:    #{valera.mana}")
-      file.puts("fun:     #{valera.fun}")
-      file.puts("fatigue: #{valera.fatigue}")
-      file.puts("money:   #{valera.money}")
+    File.open(@file, 'w+') do |file|
+      file.write valera.to_yaml
     end
   end
 end
