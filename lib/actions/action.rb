68b7efbd69da6fca1e64f8482666e86b820d77b3
Denis Dyachkov <denis.dyachkov@softomate.com>
2019-10-26 10:48:34 +0700

    add required parameter for action

--- lib/actions/action.rb
+++ lib/actions/action.rb
@@ -1,7 +1,8 @@
 class Action
   attr_accessor :name, :conditions, :effects
 
-  def initialize(effects: [], conditions: [])
+  def initialize(name:, effects:, conditions: [])
+    self.name = name
     self.effects = effects
     self.conditions = conditions
   end
@@ -12,22 +13,5 @@ class Action
 
   def run(valera)
     effects.each { |e| e.apply valera } if valid? valera
-  end
-end
-
-class ActionEffect
-  attr_accessor :conditions, :effect
-
-  def initialize(effect:, conditions: [])
-    self.effect = effect
-    self.conditions = conditions
-  end
-
-  def ok?(valera)
-    conditions.all? { |c| c.valid?(valera) }
-  end
-
-  def apply(valera)
-    effect.apply valera if ok? valera
   end
 end
