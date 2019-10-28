803f9f607506dc2177ed1c09cca43e132b174682
Aleksey Gromov <gromov-aleksey@outlook.com>
2019-10-28 22:51:58 +0700

    Валидация id

--- lib/states/select_action.rb
+++ lib/states/select_action.rb
@@ -2,14 +2,19 @@ module GameStates
   class SelectAction < Base
     def render
       puts 'Select action:'
-      context[:actions].each_with_index { |action, id| puts "#{id}) #{action.name}" }
+      context[:actions].each_with_index do |action, id|
+        id = id + 1
+        puts "#{id}) #{action.name}"
+      end
     end
 
     def next
-      #todo: валидация id
-      action_id = gets.to_i
-      context[:actions][action_id].run context[:valera]
-      CheckValera.new context
+      if (action_id = gets.to_i).between?(1, 8)
+        context[:actions][action_id].run context[:valera]
+        CheckValera.new context
+      else
+        puts 'Invalid action!'
+      end
     end
   end
 end
