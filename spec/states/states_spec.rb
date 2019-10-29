8da9a06a9709b2a543f98816aefb0604fda00f1e
Aleksey Gromov <gromov-aleksey@outlook.com>
2019-10-29 12:24:57 +0700

    Вас посетил рубокоп, на этот раз без штрафов

--- spec/states/states_spec.rb
+++ spec/states/states_spec.rb
@@ -1,17 +1,19 @@
-require_relative "../spec_helper"
+# frozen_string_literal: true
+
+require_relative '../spec_helper'
 
 describe States do
-  let(:file) { "test_states.yml" }
+  let(:file) { 'test_states.yml' }
   subject do
     States.new(file)
   end
-  describe "#save" do
-    context "check saving of Valera state" do
-      before { File.write("tes_states.yml", "") }
+  describe '#save' do
+    context 'check saving of Valera state' do
+      before { File.write('tes_states.yml', '') }
       let(:valera) { Valera.new(mana: 10, fun: 10, money: 10, fatigue: 10) }
-      it "save state in file" do
+      it 'save state in file' do
         subject.save(valera)
-        expect(File.size?("test_states.yml")).not_to be nil
+        expect(File.size?('test_states.yml')).not_to be nil
       end
     end
   end
